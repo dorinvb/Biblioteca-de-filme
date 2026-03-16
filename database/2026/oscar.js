bd274cc1b5aaac7ac2c96fd8c2c36b155681b136
dorinvb <dorin.birsan@yahoo.com>
2026-03-16 08:44:19 +0200

    Update Oscar nominations for 2026

--- database/2026/oscar.js
+++ database/2026/oscar.js
@@ -5,7 +5,7 @@ window.oscarData = {
         {
             nume: "Cel mai bun film",
             nominalizari: [
-                { film: "One Battle After Another", castigator: false },
+                { film: "One Battle After Another", castigator: true },
                 { film: "F1: The Movie", castigator: false },
                 { film: "Bugonia", castigator: false },
                 { film: "Hamnet", castigator: false },
@@ -23,14 +23,14 @@ window.oscarData = {
                 { film: "Sentimental Value", persoana: "Joachim Trier", castigator: false },
                 { film: "Marty Supreme", persoana: "Josh Safdie", castigator: false },
                 { film: "Hamnet", persoana: "Chloé Zhao", castigator: false },
-                { film: "One Battle After Another", persoana: "Paul Thomas Anderson", castigator: false },
+                { film: "One Battle After Another", persoana: "Paul Thomas Anderson", castigator: true },
                 { film: "Sinners", persoana: "Ryan Coogler", castigator: false }
             ]
         },
         {
             nume: "Cel mai bun actor în rol principal",
             nominalizari: [
-                { film: "Sinners", persoana: "Michael B. Jordan", castigator: false },
+                { film: "Sinners", persoana: "Michael B. Jordan", castigator: true },
                 { film: "The Secret Agent", persoana: "Wagner Moura", castigator: false },
                 { film: "Marty Supreme", persoana: "Timothée Chalamet", castigator: false },
                 { film: "Blue Moon", persoana: "Ethan Hawke", castigator: false },
@@ -42,7 +42,7 @@ window.oscarData = {
             nominalizari: [
                 { film: "If I Had Legs I'd Kick You", persoana: "Rose Byrne", castigator: false },
                 { film: "Bugonia", persoana: "Emma Stone", castigator: false },
-                { film: "Hamnet", persoana: "Jessie Buckley", castigator: false },
+                { film: "Hamnet", persoana: "Jessie Buckley", castigator: true },
                 { film: "Sentimental Value", persoana: "Renate Reinsve", castigator: false },
                 { film: "Song Sung Blue", persoana: "Kate Hudson", castigator: false }
             ]
@@ -52,7 +52,7 @@ window.oscarData = {
             nominalizari: [
                 { film: "Sentimental Value", persoana: "Stellan Skarsgård", castigator: false },
                 { film: "One Battle After Another", persoana: "Benicio Del Toro", castigator: false },
-                { film: "One Battle After Another", persoana: "Sean Penn", castigator: false },
+                { film: "One Battle After Another", persoana: "Sean Penn", castigator: true },
                 { film: "Frankenstein", persoana: "Jacob Elordi", castigator: false },
                 { film: "Sinners", persoana: "Delroy Lindo", castigator: false }
             ]
@@ -64,14 +64,14 @@ window.oscarData = {
                 { film: "Sentimental Value", persoana: "Elle Fanning", castigator: false },
                 { film: "Sentimental Value", persoana: "Inga Ibsdotter Lilleaas", castigator: false },
                 { film: "One Battle After Another", persoana: "Teyana Taylor", castigator: false },
-                { film: "Weapons", persoana: "Amy Madigan", castigator: false }
+                { film: "Weapons", persoana: "Amy Madigan", castigator: true }
             ]
         },
         {
             nume: "Cel mai bun scenariu original",
             nominalizari: [
                 { film: "Blue Moon", castigator: false },
-                { film: "Sinners", castigator: false },
+                { film: "Sinners", castigator: true },
                 { film: "Sentimental Value", castigator: false },
                 { film: "It Was Just an Accident", castigator: false },
                 { film: "Marty Supreme", castigator: false }
@@ -84,14 +84,14 @@ window.oscarData = {
                 { film: "Train Dreams", castigator: false },
                 { film: "Hamnet", castigator: false },
                 { film: "Bugonia", castigator: false },
-                { film: "One Battle After Another", castigator: false }
+                { film: "One Battle After Another", castigator: true }
             ]
         },
         {
             nume: "Cea mai bună imagine",
             nominalizari: [
                 { film: "One Battle After Another", castigator: false },
-                { film: "Sinners", castigator: false },
+                { film: "Sinners", castigator: true },
                 { film: "Train Dreams", castigator: false },
                 { film: "Marty Supreme", castigator: false },
                 { film: "Frankenstein", castigator: false }
@@ -104,13 +104,13 @@ window.oscarData = {
                 { film: "Sentimental Value", castigator: false },
                 { film: "Marty Supreme", castigator: false },
                 { film: "Sinners", castigator: false },
-                { film: "One Battle After Another", castigator: false }
+                { film: "One Battle After Another", castigator: true }
             ]
         },
         {
             nume: "Cea mai bună scenografie",
             nominalizari: [
-                { film: "Frankenstein", castigator: false },
+                { film: "Frankenstein", castigator: true },
                 { film: "Hamnet", castigator: false },
                 { film: "One Battle After Another", castigator: false },
                 { film: "Marty Supreme", castigator: false },
@@ -124,13 +124,13 @@ window.oscarData = {
                 { film: "Avatar: Fire and Ash", castigator: false },
                 { film: "Hamnet", castigator: false },
                 { film: "Sinners", castigator: false },
-                { film: "Frankenstein", castigator: false }
+                { film: "Frankenstein", castigator: true }
             ]
         },
         {
             nume: "Cel mai bun machiaj",
             nominalizari: [
-                { film: "Frankenstein", castigator: false },
+                { film: "Frankenstein", castigator: true },
                 { film: "The Smashing Machine", castigator: false },
                 { film: "The Ugly Stepsister", castigator: false },
                 { film: "Sinners", castigator: false },
@@ -144,14 +144,14 @@ window.oscarData = {
                 { film: "Bugonia", persoana: "Jerskin Fendrix", castigator: false },
                 { film: "Frankenstein", persoana: "Alexandre Desplat", castigator: false },
                 { film: "One Battle After Another", persoana: "Jonny Greenwood", castigator: false },
-                { film: "Sinners", persoana: "Ludwig Göransson", castigator: false }
+                { film: "Sinners", persoana: "Ludwig Göransson", castigator: true }
             ]
         },
         {
             nume: "Cel mai bun cântec original",
             nominalizari: [
                 { film: "Sinners", castigator: false },
-                { film: "KPop Demon Hunters", castigator: false },
+                { film: "KPop Demon Hunters", castigator: true },
                 { film: "Viva Verdi", castigator: false },
                 { film: "Train Dreams", castigator: false },
                 { film: "Diane Warren: Relentless", castigator: false }
@@ -161,7 +161,7 @@ window.oscarData = {
             nume: "Cel mai bun sunet",
             nominalizari: [
                 { film: "Sinners", castigator: false },
-                { film: "F1: The Movie", castigator: false },
+                { film: "F1: The Movie", castigator: true },
                 { film: "Frankenstein", castigator: false },
                 { film: "Sirât", castigator: false },
                 { film: "One Battle After Another", castigator: false }
@@ -174,7 +174,7 @@ window.oscarData = {
                 { film: "F1: The Movie", castigator: false },
                 { film: "Sinners", castigator: false },
                 { film: "The Lost Bus", castigator: false },
-                { film: "Avatar: Fire and Ash", castigator: false }
+                { film: "Avatar: Fire and Ash", castigator: true }
             ]
         },
         {
@@ -184,14 +184,14 @@ window.oscarData = {
                 { film: "Zootopia 2", castigator: false },
                 { film: "Amélie et la métaphysique des tubes", castigator: false },
                 { film: "Elio", castigator: false },
-                { film: "KPop Demon Hunters", castigator: false }
+                { film: "KPop Demon Hunters", castigator: true }
             ]
         },
         {
             nume: "Cel mai bun film străin",
             nominalizari: [
                 { film: "The Voice of Hind Rajab", castigator: false },
-                { film: "Sentimental Value", castigator: false },
+                { film: "Sentimental Value", castigator: true },
                 { film: "The Secret Agent", castigator: false },
                 { film: "Sirât", castigator: false },
                 { film: "It Was Just an Accident", castigator: false }
@@ -204,7 +204,7 @@ window.oscarData = {
                 { film: "Come See Me in the Good Light", castigator: false },
                 { film: "The Alabama Solution", castigator: false },
                 { film: "Cutting Through Rocks", castigator: false },
-                { film: "Mr. Nobody Against Putin", castigator: false }
+                { film: "Mr. Nobody Against Putin", castigator: true }
             ]
         },
         {
@@ -213,15 +213,8 @@ window.oscarData = {
                 { film: "The Secret Agent", castigator: false },
                 { film: "Marty Supreme", castigator: false },
                 { film: "Hamnet", castigator: false },
-                { film: "One Battle After Another", castigator: false },
+                { film: "One Battle After Another", castigator: true },
                 { film: "Sinners", castigator: false }
-            ]
-        },
-        {
-            nume: "Cel mai bun scurtmetraj de animație",
-            nominalizari: [
-                { film: "Papillon", castigator: false },
-                { film: "Forevergreen", castigator: false }
             ]
         }
     ]
